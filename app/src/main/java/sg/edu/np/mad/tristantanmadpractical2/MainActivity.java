2e883c370a14954a428bdf0da36185eea210399c
loghogjog <s10223003@connect.np.edu.sg>
2022-05-07 15:31:57 +0800

    Main portion done

--- app/src/main/java/sg/edu/np/mad/tristantanmadpractical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/tristantanmadpractical2/MainActivity.java
@@ -2,9 +2,12 @@ package sg.edu.np.mad.tristantanmadpractical2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,15 +39,25 @@ public class MainActivity extends AppCompatActivity {
                 if(!u.followed){
                     f.setText("Follow");
                     u.followed = true;
+                    Toast.makeText(getApplicationContext(),"Unfollowed", Toast.LENGTH_SHORT).show();
                 }
                 else{
                     f.setText("Followed");
                     u.followed = false;
+                    Toast.makeText(getApplicationContext(),"Followed", Toast.LENGTH_SHORT).show();
+
                 }
 
             }
         });
+        Intent i = getIntent();
+
+        String message = i.getStringExtra("newText");
+
+        TextView uName = findViewById(R.id.username);
+        uName.setText(message);
     }
+
 
 
 }
